45c9daefb2876dc543d18d5ba1a9501281bf63cb
Sawyer <90795257+sawyerscheiner@users.noreply.github.com>
2022-03-10 15:01:38 -0500

    Create prototype.js

---
+++ assets/js/prototype.js
@@ -0,0 +1,689 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Home: React.VFC = () => {
+  return (
+    <div className="home">
+      <div className="rectangle-1" />
+      <div className="rectangle-4" />
+      <div className="rectangle-3" />
+      <button className="text-1">HM Convenience Map</p>
+      <p className="text-2">Click for Printers</p>
+      <p className="text-3">Click for Chargers</p>
+      <div className="group-1">
+        <div className="rectangle-2" />
+        <img src="" />
+      </div>
+    </div>
+  )
+}
+
+.home {
+  height: 844px;
+  width: 390px;
+  background-color: #ffffff;
+}
+.rectangle-1 {
+  height: 63px;
+  width: 390px;
+  background-color: #c4c4c4;
+}
+.rectangle-4 {
+  height: 403px;
+  width: 390px;
+  background-color: #542b30;
+}
+.rectangle-3 {
+  height: 669px;
+  width: 390px;
+  background-color: #c4c4c4;
+}
+.text-1 {
+  text-align: center;
+  vertical-align: top;
+  font-size: 28px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-2 {
+  text-align: center;
+  vertical-align: top;
+  font-size: 28px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #c4c4c4;
+}
+.text-3 {
+  text-align: center;
+  vertical-align: top;
+  font-size: 28px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.group-1 {
+  height: 379px;
+  width: 390px;
+}
+.rectangle-2 {
+  height: 379px;
+  width: 390px;
+  background-color: #542b30;
+}
+.img-4 {
+  height: 323px;
+  width: 334px;
+}
+
+
+const Chargers: React.VFC = () => {
+  return (
+    <div className="chargers">
+      <div className="rectangle-1" />
+      <div className="rectangle-4" />
+      <div className="rectangle-3" />
+      <p className="text-1">Click On Text For Photos</p>
+      <p className="text-2">Back</p>
+      <p className="text-3">HM Chargers</p>
+      <p className="text-4">3. Library Rotunda (Second Floor)
+
+</p>
+      <p className="text-5">5 . (a) Computer Cart by Alumni Entrance (1st Floor)
+    
+
+
+
+</p>
+      <p className="text-6">5. (b) Computer Cart by Alumni Entrance (2nd Floor)
+    
+
+</p>
+      <p className="text-7">... More To Come
+    
+
+</p>
+      <p className="text-8">2. Tilinghast Basement, Under the Main Staircase
+
+</p>
+      <div className="group-1">
+        <div className="rectangle-2" />
+        <img src="" />
+      </div>
+      <div className="ellipse-1" />
+      <div className="ellipse-3" />
+      <div className="ellipse-2" />
+    </div>
+  )
+}
+
+.chargers {
+  height: 844px;
+  width: 390px;
+  background-color: #ffffff;
+}
+.rectangle-1 {
+  height: 63px;
+  width: 390px;
+  background-color: #c4c4c4;
+}
+.rectangle-4 {
+  height: 71px;
+  width: 390px;
+  background-color: #542b30;
+}
+.rectangle-3 {
+  height: 773px;
+  width: 390px;
+  background-color: #c4c4c4;
+}
+.text-1 {
+  text-align: center;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-2 {
+  text-align: center;
+  vertical-align: top;
+  font-size: 28px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #c4c4c4;
+}
+.text-3 {
+  text-align: center;
+  vertical-align: top;
+  font-size: 28px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-4 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: undefined;
+  line-height: auto;
+  color: #542b30;
+}
+.text-5 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-6 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-7 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-8 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: undefined;
+  line-height: auto;
+  color: #542b30;
+}
+.group-1 {
+  height: 379px;
+  width: 390px;
+}
+.rectangle-2 {
+  height: 379px;
+  width: 390px;
+  background-color: #542b30;
+}
+.img-9 {
+  height: 323px;
+  width: 334px;
+}
+.ellipse-1 {
+  height: 44px;
+  width: 44px;
+}
+.ellipse-3 {
+  height: 44px;
+  width: 44px;
+}
+.ellipse-2 {
+  transform: rotate(-180deg);
+  height: 44px;
+  width: 44px;
+}
+
+const Printers: React.VFC = () => {
+  return (
+    <div className="printers">
+      <div className="rectangle-1" />
+      <div className="rectangle-4" />
+      <div className="rectangle-3" />
+      <p className="text-1">HM Printers</p>
+      <p className="text-2">Back</p>
+      <div className="group-1">
+        <div className="rectangle-2" />
+        <img src="" />
+      </div>
+    </div>
+  )
+}
+
+.printers {
+  height: 844px;
+  width: 390px;
+  background-color: #ffffff;
+}
+.rectangle-1 {
+  height: 63px;
+  width: 390px;
+  background-color: #c4c4c4;
+}
+.rectangle-4 {
+  height: 71px;
+  width: 390px;
+  background-color: #542b30;
+}
+.rectangle-3 {
+  height: 773px;
+  width: 390px;
+  background-color: #c4c4c4;
+}
+.text-1 {
+  text-align: center;
+  vertical-align: top;
+  font-size: 28px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-2 {
+  text-align: center;
+  vertical-align: top;
+  font-size: 28px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #c4c4c4;
+}
+.group-1 {
+  height: 379px;
+  width: 390px;
+}
+.rectangle-2 {
+  height: 379px;
+  width: 390px;
+  background-color: #542b30;
+}
+.img-3 {
+  height: 323px;
+  width: 334px;
+}
+
+const 2ChargerPicture: React.VFC = () => {
+  return (
+    <div className="2-charger-picture">
+      <div className="rectangle-1" />
+      <div className="rectangle-4" />
+      <div className="rectangle-3" />
+      <p className="text-1">2. Tilinghast Basement, Under the Main Staircase</p>
+      <p className="text-2">Back</p>
+      <p className="text-3">HM Chargers</p>
+      <p className="text-4">3. Library Rotunda (Second Floor)
+
+</p>
+      <p className="text-5">7. (a) Computer Cart by Alumni Entrance (1st Floor)
+    
+
+
+
+</p>
+      <p className="text-6">    (b) Computer Cart by Alumni Entrance (2nd Floor)
+    
+
+</p>
+      <p className="text-7">... More To Come
+    
+
+</p>
+      <p className="text-8">2. Tilinghast Basement, Under the Main Staircase
+
+</p>
+      <div className="group-1">
+        <div className="rectangle-2" />
+      </div>
+      <img src="" />
+    </div>
+  )
+}
+
+.2-charger-picture {
+  height: 844px;
+  width: 390px;
+  background-color: #ffffff;
+}
+.rectangle-1 {
+  height: 63px;
+  width: 390px;
+  background-color: #c4c4c4;
+}
+.rectangle-4 {
+  height: 71px;
+  width: 390px;
+  background-color: #542b30;
+}
+.rectangle-3 {
+  height: 773px;
+  width: 390px;
+  background-color: #c4c4c4;
+}
+.text-1 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-2 {
+  text-align: center;
+  vertical-align: top;
+  font-size: 28px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #c4c4c4;
+}
+.text-3 {
+  text-align: center;
+  vertical-align: top;
+  font-size: 28px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-4 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: undefined;
+  line-height: auto;
+  color: #542b30;
+}
+.text-5 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-6 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-7 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-8 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: undefined;
+  line-height: auto;
+  color: #542b30;
+}
+.group-1 {
+  height: 595px;
+  width: 390px;
+}
+.rectangle-2 {
+  height: 595px;
+  width: 390px;
+  background-color: #542b30;
+}
+
+const 3ChargerPicture: React.VFC = () => {
+  return (
+    <div className="3-charger-picture">
+      <div className="rectangle-1" />
+      <div className="rectangle-4" />
+      <div className="rectangle-3" />
+      <p className="text-1">3. Library Rotunda (Second Floor)</p>
+      <p className="text-2">Back</p>
+      <p className="text-3">HM Chargers</p>
+      <p className="text-4">3. Library Rotunda (Second Floor)
+
+</p>
+      <p className="text-5">7. (a) Computer Cart by Alumni Entrance (1st Floor)
+    
+
+
+
+</p>
+      <p className="text-6">    (b) Computer Cart by Alumni Entrance (2nd Floor)
+    
+
+</p>
+      <p className="text-7">... More To Come
+    
+
+</p>
+      <p className="text-8">2. Tilinghast Basement, Under the Main Staircase
+
+</p>
+      <div className="group-1">
+        <div className="rectangle-2" />
+      </div>
+      <img src="" />
+    </div>
+  )
+}
+
+.3-charger-picture {
+  height: 844px;
+  width: 390px;
+  background-color: #ffffff;
+}
+.rectangle-1 {
+  height: 63px;
+  width: 390px;
+  background-color: #c4c4c4;
+}
+.rectangle-4 {
+  height: 71px;
+  width: 390px;
+  background-color: #542b30;
+}
+.rectangle-3 {
+  height: 773px;
+  width: 390px;
+  background-color: #c4c4c4;
+}
+.text-1 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-2 {
+  text-align: center;
+  vertical-align: top;
+  font-size: 28px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #c4c4c4;
+}
+.text-3 {
+  text-align: center;
+  vertical-align: top;
+  font-size: 28px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-4 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: undefined;
+  line-height: auto;
+  color: #542b30;
+}
+.text-5 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-6 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-7 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-8 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: undefined;
+  line-height: auto;
+  color: #542b30;
+}
+.group-1 {
+  height: 595px;
+  width: 390px;
+}
+.rectangle-2 {
+  height: 595px;
+  width: 390px;
+  background-color: #542b30;
+}
+
+const 3ChargerPicture: React.VFC = () => {
+  return (
+    <div className="3-charger-picture">
+      <div className="rectangle-1" />
+      <div className="rectangle-4" />
+      <div className="rectangle-3" />
+      <p className="text-1">3. Library Rotunda (Second Floor)</p>
+      <p className="text-2">Back</p>
+      <p className="text-3">HM Chargers</p>
+      <p className="text-4">3. Library Rotunda (Second Floor)
+
+</p>
+      <p className="text-5">7. (a) Computer Cart by Alumni Entrance (1st Floor)
+    
+
+
+
+</p>
+      <p className="text-6">    (b) Computer Cart by Alumni Entrance (2nd Floor)
+    
+
+</p>
+      <p className="text-7">... More To Come
+    
+
+</p>
+      <p className="text-8">2. Tilinghast Basement, Under the Main Staircase
+
+</p>
+      <div className="group-1">
+        <div className="rectangle-2" />
+      </div>
+      <img src="" />
+    </div>
+  )
+}
+
+.3-charger-picture {
+  height: 844px;
+  width: 390px;
+  background-color: #ffffff;
+}
+.rectangle-1 {
+  height: 63px;
+  width: 390px;
+  background-color: #c4c4c4;
+}
+.rectangle-4 {
+  height: 71px;
+  width: 390px;
+  background-color: #542b30;
+}
+.rectangle-3 {
+  height: 773px;
+  width: 390px;
+  background-color: #c4c4c4;
+}
+.text-1 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-2 {
+  text-align: center;
+  vertical-align: top;
+  font-size: 28px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #c4c4c4;
+}
+.text-3 {
+  text-align: center;
+  vertical-align: top;
+  font-size: 28px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-4 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: undefined;
+  line-height: auto;
+  color: #542b30;
+}
+.text-5 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-6 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-7 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: Roboto;
+  line-height: auto;
+  color: #542b30;
+}
+.text-8 {
+  text-align: left;
+  vertical-align: top;
+  font-size: 18px;
+  font-family: undefined;
+  line-height: auto;
+  color: #542b30;
+}
+.group-1 {
+  height: 595px;
+  width: 390px;
+}
+.rectangle-2 {
+  height: 595px;
+  width: 390px;
+  background-color: #542b30;
+}
